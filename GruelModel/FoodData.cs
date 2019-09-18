2097052c32c525eea202f1f9b715ba5719262cba
jungwoo5042 <42081803+TrollShin@users.noreply.github.com>
2019-09-18 11:21:02 +0900

    190918 메뉴제작

--- GruelModel/FoodData.cs
+++ GruelModel/FoodData.cs
@@ -18,7 +18,13 @@ namespace GruelModel
 
             listFood = new List<Food>()
             {
-                new Food() {Name = "신불닭죽", Price = 10000, ImagePath = "Assets/신불닭죽_pic.jpg", category = SIGNATURE},
+                //메뉴의 카테고리 (0.시그니처 1.보양 2.영양 3.별미 4.전통 5.반찬)
+                new Food() {Name = "신불닭죽", Price = 10000, ImagePath = "Assets/신불닭죽_pic.jpg", category = Category.DELICACY},
+                new Food() {Name = "둥지팥죽", Price = 9000, ImagePath = "Assets/둥지팥죽_pic.jpg", category = Category.TRADITION},
+                new Food() {Name = "든든한끼장조림", Price = 2900, ImagePath = "Assets/든든한끼장조림_pic.jpg", category = Category.SIDEDISH},
+                new Food() {Name = "쇠고기미역죽", Price = 8500, ImagePath = "Assets/쇠고기미역죽_pic.jpg", category = Category.NUTRITION},
+                new Food() {Name = "삼계전복죽", Price = 15000, ImagePath = "Assets/삼계전복죽_pic.jpg", category = Category.RECUPERATION},
+                new Food() {Name = "트러플전복죽", Price = 16000, ImagePath = "Assets/트러플전복죽_pic.jpg", category = Category.SIGNATURE},
             };
 
             isLoaded = true;
