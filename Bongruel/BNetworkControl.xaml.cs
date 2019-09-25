2c69cd3c27e1c423d32784c46025270c1ce2f9c4
jungwoo5042 <42081803+TrollShin@users.noreply.github.com>
2019-09-25 09:21:29 +0900

    MainWindow 수정 BNetwork가 빠짐

--- Bongruel/BNetworkControl.xaml.cs
+++ Bongruel/BNetworkControl.xaml.cs
@@ -16,17 +16,15 @@ using System.Windows.Shapes;
 namespace Bongruel
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for BNetworkControl.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class BNetworkControl : UserControl
     {
-        public MainWindow()
+        public BNetworkControl()
         {
             InitializeComponent();
         }
-
-        //BNetwork로 넘어감
-        /*private void BtnSend_Click(object sender, RoutedEventArgs e)
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -36,27 +34,11 @@ namespace Bongruel
                 bNetwork.Send(tbInput.Text);
             }
 
-            catch(Exception)
+            catch (Exception)
             {
                 MessageBox.Show("없음 서버 응답 요청 시작 다시");
             }
-        }*/
 
-        private void GoMenuWindowBtn_Click(object sender, RoutedEventArgs e)
-        {
-            changeUserControl(new MenuWindow());
-        }
-
-        private void GoBNetwork_Click(object sender, RoutedEventArgs e)
-        {
-            changeUserControl(new BNetworkControl());
-        }
-
-        //임시
-        private void changeUserControl(UserControl userControl)
-        {
-            buttonGrid.Visibility = Visibility.Collapsed;
-            contentControl.Content = userControl;
         }
     }
 }
