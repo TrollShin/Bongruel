b0cfb8348fc104f9bafa5806d70ca97c3ee782af
HelloMan <37075549+rnysy@users.noreply.github.com>
2019-10-16 09:27:45 +0900

    로딩바 구현

---
+++ Bongruel/LoadingControl.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Bongruel
+{
+    /// <summary>
+    /// LoadingControl.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoadingControl : UserControl, INotifyPropertyChanged
+    {
+        private BackgroundWorker bgWorker = new BackgroundWorker();
+
+        private int workerState;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int WorkerState
+        {
+            get { return workerState; }
+            set
+            {
+                workerState = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("WorkerState"));
+            }
+        }
+        public LoadingControl()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            bgWorker.DoWork += (s, e) =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    System.Threading.Thread.Sleep(100);
+                    WorkerState = i;
+                }
+                MessageBox.Show("로딩 완료!");
+            };
+            bgWorker.RunWorkerAsync();
+        }
+    }
+}
