be0f3d6c11f2160809407207663e90b0624eb831
HelloMan <37075549+rnysy@users.noreply.github.com>
2019-11-07 14:48:11 +0900

    무수정

--- Bongruel/App.xaml.cs
+++ Bongruel/App.xaml.cs
@@ -16,6 +16,22 @@ namespace Bongruel
     {
         public static FoodData foodData = new FoodData();
         public static SeatData seatData = new SeatData();
-        public static StatData statData = new StatData();       
+        public static StatData statData = new StatData();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            //SplashScreen splash = new SplashScreen("SplashScreen.png");
+            //splash.Show(true);
+            //MainWindow main = new MainWindow();
+            //splash.Close(new TimeSpan(0, 0, 1));
+            //Task<MainWindow> windowTask = main.Show();
+            //await windowTask;
+        }
+
+        //private async Task Application_Startup(object sender, StartupEventArgs e)
+        // {
+
+
+        //}
     }
 }
